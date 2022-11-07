30342ce931f1b1964baec99c32513bfdf2b55ff5
Your Name <phamtienquan2001@gmail.com>
2022-11-07 18:21:17 +0700

    done header and footer

---
+++ fe/src/components/header/HeaderComponent.jsx
@@ -0,0 +1,175 @@
+import logo from '../../assets/themes/icons/logo.svg';
+import {useState} from "react";
+ const HeaderComponent=({onClickAside})=>{
+
+  return (
+    <header className="clearfix">
+      <a  id="togger__menu" onClick={()=>onClickAside()}>
+        <span></span>
+        <span></span>
+      </a>
+      <div className="container">
+        <div className="header__left">
+          <a href="" style={{display:"none"}}>SELL</a>
+          <a href="https://www.glab.vn/user/items" style={{display:"none"}}>CONSIGNMENT MANAGER</a>
+        </div>
+        <div className="header__right">
+          <div className="auth__user">
+            <div className="dropdown">
+              <a href="" className="toggle__auth val-selected"><span className="icon-slice6"></span></a>
+              <div className="dropdown-up-style hide">
+                <div className="dropdown__inner">
+                  <ul>
+                    <li><a href="https://www.glab.vn/login">Log In</a></li>
+                    <li><a href="https://www.glab.vn/register">Create Account</a></li>
+                    <li><a href="https://www.glab.vn/contract/find">Find Contract</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="" className="header__right--mbsearch" style={{display:"inline-block"}}><span className="icon-search"></span></a>
+          <div className="header__cart dropdown">
+            <a href="" className="val-selected"><span className="icon-slice8"></span><span
+              className="header__cart--num hide">()</span></a>
+            <div className="dropdown-up-style hide">
+              <div className="dropdown__inner">
+                <h2 className="text-uper font-700 fs-16">your cart</h2>
+                <div className="text-center">
+                  <p className="font-500 fs-15 mgB-15">Không có sản phẩm nào !</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center header__logo">
+          <span>logo</span>
+          <a href="https://www.glab.vn"><img src={logo} alt="" /></a>
+        </div>
+      </div>
+      <div className="container">
+        <div id="menu">
+          <div className="inner-menu">
+            <div>
+              <ul>
+                <li>
+                  <a href="https://www.glab.vn/product/features"><span>Features</span></a>
+                </li>
+                <li>
+                  <a href="https://www.glab.vn/product/footwear"><span>Footwear</span></a>
+                  <div className="menu__sub">
+                    <ul>
+                      <li>
+                        <a href="https://www.glab.vn/product/newest-sneakers">Newest Sneakers</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/lifestyle">Lifestyle</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/running">Running</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/basketball">Basketball</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/gym-training">Gym &amp; Training</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/sandal">Sandal</a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <a href="https://www.glab.vn/product/apparel"><span>Apparel</span></a>
+                  <div className="menu__sub">
+                    <ul>
+                      <li>
+                        <a href="https://www.glab.vn/product/conic" style={{color:"red"}}>CONIC®</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/glab" style={{color:"red"}}>GLAB®</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/hoodies">Hoodies</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/shorts">Shorts</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/outerwears">Outerwears</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/tees">Tees</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/pants">Pants</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/shirts">Shirts</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/jackets">Jackets</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/sweaters">Sweaters</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/long-sleeves">Long Sleeves</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/vintage">Vintage</a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <a href="https://www.glab.vn/product/accessories"><span>Accessories</span></a>
+                  <div className="menu__sub">
+                    <ul>
+                      <li>
+                        <a href="https://www.glab.vn/product/watch">Watch</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/socks">Socks</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/cap">Cap</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/backpack-and-bag">Backpack and Bag</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/shoe-care">Shoe Care</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/miscellaneous">Miscellaneous</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/glasses">Glasses</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/bearbrick">Bearbrick</a>
+                      </li>
+                      <li>
+                        <a href="https://www.glab.vn/product/face-mask">Face Mask</a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <a href="https://www.glab.vn/product/type/used"><span>Used</span></a>
+                </li>
+                <li>
+                  <a href="https://www.glab.vn/product/type/sale"><span>Sale</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    )
+}
+
+export default HeaderComponent
--- fe/src/layouts/mainlayout/MainLayout.jsx
+++ fe/src/layouts/mainlayout/MainLayout.jsx
@@ -1,17 +1,46 @@
+import {Drawer, Layout} from 'antd';
+import HeaderComponent from "../../components/header/HeaderComponent";
 
-import {BackTop, Drawer, Layout, Menu, Switch} from 'antd';
+import {Row} from "react-bootstrap";
+import {useState} from "react";
+import AsideComponent from "../../components/aside/AsideComponent";
+import FooterComponent from "../../components/footer/FooterComponent";
 
 
-const { Header, Footer, Sider, Content } = Layout;
+const {Header, Footer, Sider, Content} = Layout;
 
-const MainLayout=()=>{
-    return(
-      <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    )
+const MainLayout = () => {
+
+  const [closeAside, setCloseAside] = useState(false);
+  const onclickCloseAside = () => {
+    setCloseAside(!closeAside)
+  };
+  const onClose = () => {
+    setCloseAside(false)
+  };
+  console.log(closeAside)
+  return (
+    <div id="wrapper">
+      <Row xl={12} sm={12} md={12} xs={12} lg={12} xxl={12}>
+        {/*<Layout style={{display: "flex", flex: 1, height: "auto"}}>
+          <Drawer
+            placement="left"
+            closable={false}
+            onClose={onClose}
+            open={closeAside}
+            width="300px"
+          >
+            <AsideComponent onClose={onClose}/>
+          </Drawer>
+        </Layout>*/}
+        <Layout>
+          <HeaderComponent onClickAside={onclickCloseAside}/>
+          <Content style={{minHeight: "1500px"}}></Content>
+          <FooterComponent/>
+        </Layout>
+      </Row>
+    </div>
+  )
 }
 
 
